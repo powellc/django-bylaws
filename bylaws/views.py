adb5a2cef699d43a1d7da49e95ce874a5c480ac0
Colin Powell <colin@onecardinal.com>
2011-03-10 21:20:39 -0500

    Removing cruft from previous project and first stab at proper views.

--- bylaws/views.py
+++ bylaws/views.py
@@ -1,62 +1,41 @@
 from django.shortcuts import render_to_response, get_object_or_404
-from django.views.generic.create_update import delete_object
-from django.http import HttpResponseRedirect, Http404, HttpResponse
-from django.template import RequestContext
-from django.core.urlresolvers import reverse
-from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-from django.views.generic.create_update import delete_object
-import datetime
+from django.template.context import RequestContext
 
-from bylaws.forms import EventForm, OccurrenceForm
 from bylaws.models import Bylaws, UserSignature
 
-@login_required
-def signature_detail(request, user, login_required=True, extra_context=None):
-    extra_context = extra_context or {}
-    if request.user == user:
-    signature = get_object_or_404(Event, id=event_id)
-    next = next or reverse('day_calendar', args=[event.calendar.slug])
-    next = get_next_url(request, next)
-    extra_context['next'] = next
-    return delete_object(request,
-                         model = Event,
-                         object_id = event_id,
-                         post_delete_redirect = next,
-                         template_name = "schedule/delete_event.html",
-                         extra_context = extra_context,
-                         login_required = login_required
-                        )
+def current_bylaws(request):
+    object = Bylaws.adopted_objects.latest()
+    return render_to_response('bylaws/bylaws_detail.html', locals(),
+                              context_instance=RequestContext(request))
+    
+def bylaws_list(request):
+    objects = Bylaws.objects.all()
+    return render_to_response('bylaws/bylaws_list.html', locals(),
+                              context_instance=RequestContext(request))
 
-@login_required
-def signature_detail(request, user, login_required=True, extra_context=None):
-    extra_context = extra_context or {}
-    event = get_object_or_404(Event, id=event_id)
-    next = next or reverse('day_calendar', args=[event.calendar.slug])
-    next = get_next_url(request, next)
-    extra_context['next'] = next
-    return delete_object(request,
-                         model = Event,
-                         object_id = event_id,
-                         post_delete_redirect = next,
-                         template_name = "schedule/delete_event.html",
-                         extra_context = extra_context,
-                         login_required = login_required
-                        )
+def bylaws_diff_list(request, year_month_slug):
+    '''
+    Returns an object looked up by when it was created so that it's history can be displayed and diffed
+    '''
+    year, month = year_month_slug.split('-')
+    object = Bylaws.objects.get(created__year=year, created__month=month)
+    try:
+        history= object.history.all()
+    except:
+        history= None 
+    return render_to_response('bylaws/bylaws_diff_list.html', locals(),
+                              context_instance=RequestContext(request))
 
-def check_next_url(next):
-    """
-    Checks to make sure the next url is not redirecting to another page.
-    Basically it is a minimal security check.
-    """
-    if not next or '://' in next:
-        return None
-    return next
+def bylaws_diff_detail(request, year_month_slug, version_id):
+    '''
+    Returns an object looked up by when it was created so that it's history can be displayed and diffed
+    '''
+    year, month = year_month_slug.split('-')
+    object = Bylaws.objects.get(created__year=year, created__month=month)
+    try:
+        history= object.diff(object.history.all()[version_id])
+    except:
+        history= None 
+    return render_to_response('bylaws/bylaws_diff_detail.html', locals(),
+                              context_instance=RequestContext(request))
 
-def get_next_url(request, default):
-    next = default
-    if OCCURRENCE_CANCEL_REDIRECT:
-        next = OCCURRENCE_CANCEL_REDIRECT
-    if 'next' in request.REQUEST and check_next_url(request.REQUEST['next']) is not None:
-        next = request.REQUEST['next']
-    return next
