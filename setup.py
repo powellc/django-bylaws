8f8ea8a49eaeff232e7adc28f428790ed34b8726
Colin Powell <colin@onecardinal.com>
2011-04-21 00:48:45 -0400

    Fixing typo in setup file and upping version.

--- setup.py
+++ setup.py
@@ -1,24 +1,36 @@
 from setuptools import setup, find_packages
- 
+
 setup(
     name='django-bylaws',
-    version='0.1.0',
-    description='Manage bylaws in on a Django-powered site.',
+    version=__import__('bylaws').__version__,
+    license="BSD",
+
+    install_requires = [
+        'django-markup-mixin',
+        'django-extensions',
+        'simple_history',],
+
+    description='Manages bylaws for a website built on Django.',
+    long_description=open('README.md').read(),
+
     author='Colin Powell',
     author_email='colin@onecardinal.com',
-    url='http://github.com/powellc/django-bylaws/',
-    packages=find_packages(),
+
+    url='http://github.com/powellc/django-bylaws',
+    download_url='http://github.com/powellc/django-bylaws/downloads',
+
+    include_package_data=True,
+
+    packages=['bylaws'],
+
+    zip_safe=True,
     classifiers=[
-        'Development Status :: Alpha',
+        'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Framework :: Django',
-    ],
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['setuptools'],
+    ]
 )
-
